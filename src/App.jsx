20161b212aefcb74ce5f54a344a74f3e18919cd0
DhanushShettyH <dhanushtruth@gmail.com>
2025-06-10 07:36:49 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+import React, { useEffect } from "react";
+
+export default function App() {
+  useEffect(() => {
+    window.location.href = "/Dhanush_Resume.pdf";
+  }, []);
+
+  return null;
+}
